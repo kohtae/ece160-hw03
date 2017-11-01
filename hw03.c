e72fe5bcb7d356d3b050c39a023e5dad64adda94
kohtae <31904115+kohtae@users.noreply.github.com>
2017-10-31 20:33:34 -0400

    Update hw03.c

--- hw03.c
+++ hw03.c
@@ -1,5 +1,3 @@
-#include <stdio.h>
-
 typedef enum {
   NAME_ONLY = 0,
   MAJOR_AND_NAME = 1, 
@@ -10,6 +8,9 @@ typedef struct {
   char *first_name;
   char *last_name;
   // TODO: add fields here for major and year
+  char *major;
+  int year;
+
 } Student;
 
 void print_student(Mode m, Student s) {
@@ -17,7 +18,13 @@ void print_student(Mode m, Student s) {
     case NAME_ONLY:
       printf("%s %s\n", s.first_name, s.last_name);
       break;
-	// TODO: handle other cases
+    // TODO: handle other cases
+    case MAJOR_AND_NAME:
+    printf("%s %s &s\n", s.major, s.first_name, s.last_name);
+    break;
+    case YEAR_AND_NAME:
+    printf("%d %s %s \n", s.year, s.first_name, s.last_name);
+    break;
   }
 }
 
@@ -27,16 +34,33 @@ void print_student(Mode m, Student s) {
 	the fields are all strings (char[])
 	except year is a number (int)
 */
-int main() {
+int main(int argc, char* argv[]) {
   // TODO: parse argv to populate student structs 
   // for now, here's two hardcoded students:
-  Student s1, s2;
-  s1.first_name = "Julia";
-  s1.last_name = "A";
-  s2.first_name = "Peter";
-  s2.last_name = "Cooper";
-  
-  print_student(NAME_ONLY, s1);
-  print_student(NAME_ONLY, s2);
+  int count_student = argc - 2, count_type = 1 ;
+
+  while ( 0 < count_student){
+    int name1 = count_type + 1;
+    int name2 = count_type + 2;
+    int mjr = count_type + 3;
+    int yr = count_type + 4;
+    Student s1;
+    s1.first_name = argv[name1];
+    s1.last_name = argv[name2];
+    s1.major = argv[mjr];
+    s1.year = atoi(yr]);
+    if (atoi(argv[1]) == 0){
+      print_student(NAME_ONLY, s1);
+    }
+    else if (atoi(argv[1]) == 1);{
+      print_student(MAJOR_AND_NAME, s1);
+    }
+    else if (atoi(argv[1]) == 2){
+      print_student(YEAR_AND_NAME, s1);
+    }
+count_student = count_student - 4;
+count_type = count_type + 4;
+
+  }
   return 0;
 }
